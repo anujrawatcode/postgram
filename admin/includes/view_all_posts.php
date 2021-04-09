7b51a4efb47bc2c68ab2c16f1f9a5bb08bd9c3ef
AdarshKumarGEU <as195058@gmail.com>
2021-04-09 12:36:02 +0530

    14.8

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -28,7 +28,7 @@
                                 $post_image = $row['post_image'];
                                 $post_tags = $row['post_tags'];
                                 $post_comment_count = $row['post_comment_count'];
-
+                                $post_date =$row['post_date'];
 
                                 echo "<tr>";
                                 echo "<td>$post_id</td>";
@@ -37,8 +37,11 @@
                                 echo "<td>$post_category_id</td>";
                                 echo "<td>$post_status</td>";
                                 echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
-                                echo "<td>$post_tags</td>";
+                                echo "<td>$post_tags</td>"; 
                                 echo "<td>$post_comment_count</td>";
+                                echo "<td>$post_date</td>";
+                                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>EDIT</a> </td>";
+                                echo "<td><a href='posts.php?delete={$post_id}'>Delete</a> </td>";
                                 echo "</tr>";
                             }
 
@@ -46,3 +49,13 @@
                            
                         </tbody>
                     </table>
+
+ <?php
+ if(isset($_GET['delete'])){
+
+    $the_post_id=$_GET['delete'];
+    $query="DELETE FROM posts WHERE post_id={$the_post_id}";
+    $delete_query=mysqli_query($connection,$query);
+
+ }
+ ?>
